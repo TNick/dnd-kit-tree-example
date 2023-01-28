b85a581f112f652ecf45b5cecd3937eed7f0ec19
Nicu Tofan <nicu.tofan@gmail.com>
2023-01-28 22:15:40 +0200

    describe Action

--- src/original/components/Action/Action.tsx
+++ src/original/components/Action/Action.tsx
@@ -1,33 +1,55 @@
-import React, {forwardRef, CSSProperties} from 'react';
+import React, {
+  forwardRef, CSSProperties, HTMLAttributes
+} from 'react';
 import classNames from 'classnames';
 
 import './Action.css';
 
-export interface Props extends React.HTMLAttributes<HTMLButtonElement> {
+
+/**
+ * Properties expected by the Action component.
+ */
+export interface Props extends HTMLAttributes<HTMLButtonElement> {
+  /**
+   * Styles override for when the button/action is pressed
+   * (fill is used for the color of the svg icon).
+   */
   active?: {
     fill: string;
     background: string;
   };
+
+  /**
+   * The shape of the mouse pointer.
+   */
   cursor?: CSSProperties['cursor'];
 }
 
+
+/**
+ * A button with customizable pressed apparence.
+ * 
+ * This is the base for other buttons in the application.
+ * 
+ * Style accepts `--action-background` asvariable for
+ * hover background color customization.
+ * 
+ */
 export const Action = forwardRef<HTMLButtonElement, Props>(
-  ({active, className, cursor, style, ...props}, ref) => {
-    return (
-      <button
-        ref={ref}
-        {...props}
-        className={classNames('Action', className)}
-        tabIndex={0}
-        style={
-          {
-            ...style,
-            cursor,
-            '--fill': active?.fill,
-            '--background': active?.background,
-          } as CSSProperties
-        }
-      />
-    );
-  }
+  ({ active, className, cursor, style, ...props }, ref) => (
+    <button
+      ref={ref}
+      {...props}
+      className={classNames('Action', className)}
+      tabIndex={0}
+      style={
+        {
+          ...style,
+          cursor,
+          '--fill': active?.fill,
+          '--background': active?.background,
+        } as CSSProperties
+      }
+    />
+  )
 );
