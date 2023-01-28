91d8f753c2b8a225b69705e57c05175d12d2f44f
Nicu Tofan <nicu.tofan@gmail.com>
2023-01-28 21:05:07 +0200

    add some data to use in the tree

---
+++ src/application/data/original-data.ts
@@ -0,0 +1,31 @@
+import { TreeItems } from "../../original/types";
+
+/**
+ * This is the data used in the dnd-kit example.
+ */
+export const originalData: TreeItems = [
+    {
+        id: 'Home',
+        children: [],
+    },
+    {
+        id: 'Collections',
+        children: [
+            { id: 'Spring', children: [] },
+            { id: 'Summer', children: [] },
+            { id: 'Fall', children: [] },
+            { id: 'Winter', children: [] },
+        ],
+    },
+    {
+        id: 'About Us',
+        children: [],
+    },
+    {
+        id: 'My Account',
+        children: [
+            { id: 'Addresses', children: [] },
+            { id: 'Order History', children: [] },
+        ],
+    },
+];
